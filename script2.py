efe44959394c7e8cc5df184d27017fbdf0fa85fe
Darsh Solanki <Darsh.Solanki@flemingcollege.ca>
2023-05-17 16:03:48 -0400

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 # Print lines from Zen of Python
 print("Althrough that way may not be obvious at first unless you're Dutch.")
+print("If the impementation is easy to explain, it may be a good idea.")
+print("Namespaces are one honking great idea -- let's do more of those!")
